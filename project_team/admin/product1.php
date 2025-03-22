d8449561372f152d5f40f8e3987c99d06ad0918f
morn sovathana <mornsovathana@gmail.com>
2025-03-22 10:42:32 +0700

    update main product UI

--- project_team/admin/product1.php
+++ project_team/admin/product1.php
@@ -2,8 +2,8 @@
     <!-- start -->
     <div class = "h-full px-2 py-2 bg-blue-50">
         <div class = "w-full h-[100vh] bg-[#ffffff] rounded-md p-5">
-            <div class = "w-full h-[80px] grid grid-cols-2 md:grid-cols-4 gap-5 ">
-                <div class = "bg-gray-800 h-full flex  justify-center items-center flex-col text-white rounded-md text-[15px]">
+            <div class = "w-full h-[140px] md:h-[70px] grid grid-cols-2 sm:grid-cols-5 sm:h-[70px] md:grid-cols-6 gap-5 ">
+                <div class = "bg-gray-800 h-full flex  justify-center items-center flex-col text-white rounded-md sm:text-[11px] text-[10px]">
                 <p>Total Main Product</p>
                 <?php
                 $count = countTable("product_database");
@@ -12,7 +12,7 @@
                 <?php
                 ?>
                 </div>  
-                <div class = "bg-gray-800 h-full flex  justify-center items-center flex-col text-white rounded-md text-[15px]">
+                <div class = "bg-gray-800 h-full flex  justify-center items-center flex-col text-white rounded-md sm:text-[11px] text-[10px]">
                 <p>Total Sub Category</p>
                 <?php
                 $count = countTable("category_db");
@@ -21,7 +21,7 @@
                 <?php
                 ?>
                 </div>  
-                <div class = "bg-gray-800 h-full flex  justify-center items-center flex-col text-white rounded-md text-[15px]">
+                <div class = "bg-gray-800 h-full flex  justify-center items-center flex-col text-white rounded-md sm:text-[11px] text-[10px]">
                 <p>Brands</p>
                 <?php
                 $count = countTable("category_db");
@@ -31,14 +31,14 @@
                 ?>
                 </div> 
             </div>
-            <div class = "w-full h-full mt-5">
-            <table class = "text-center w-full">
+            <div class = "w-full h-full mt-5 overflow-x-auto">
+            <table class = "text-center w-full table-auto">
                     <thead>
                         <tr>
-                            <th class = "text-sm py-1 border border-gray-200 font-medium">ID</th>
-                            <th class = "text-sm py-1 border border-gray-200 font-medium">Product Name</th>
-                            <th width = "400" class = "text-sm py-1 border border-gray-200 font-medium">Total Sub Category</th>
-                            <th width = "160" class = "text-sm py-1 border border-gray-200 font-medium">Option</th>
+                            <th width = "30" class = "py-1 text-[11px] md:text-[13px] border border-gray-200 font-medium">ID</th>
+                            <th width = "50" class = "py-1 text-[11px] md:text-[13px] border border-gray-200 font-medium">Name</th>
+                            <th width = "300" class = "py-1 text-[11px] md:text-[13px] border border-gray-200 font-medium">Total Category</th>
+                            <th width = "160" class = "py-1 text-[11px] md:text-[13px] border border-gray-200 font-medium">Option</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -48,9 +48,9 @@
                             foreach($product_title as $product){
                                 ?>
                             <tr id = "product-<?= $product['id']?>">
-                            <td class = "text-sm py-1 border border-gray-200"><?= $product['id']?></td>
-                            <td id = "productName" class = "text-sm py-1 border border-gray-200"><?= $product['product_name']?></td>
-                            <td class = "text-sm py-1 border border-gray-200"><span id = "circle" class = "text-sm py-1 border border-gray-200">
+                            <td class = "text-[11px] md:text-[13px] py-1 border border-gray-200"><?= $product['id']?></td>
+                            <td id = "productName" class = "text-[11px] md:text-[13px] py-1 border border-gray-200"><?= $product['product_name']?></td>
+                            <td class = "text-[11px] md:text-[13px] py-1 border border-gray-200"><span id = "circle" class = "text-[11px] md:text-[13px] py-1 border border-gray-200">
                                 <?php
                                     $count = countCategoryItem("category_db",$product['product_name']);
                                     ?>
@@ -58,9 +58,9 @@
                                     <?php
                                 ?>
                             </span></td>
-                            <td class = "border border-gray-200 text-sm py-1">
-                                <button class = "bg-blue-500 text-white px-2 py-[1px] text-sm  rounded-md" id = "productEdit" data-id = "<?= $product['id']?>"><i class="fas fa-edit"></i></button>
-                                <button class = "bg-red-500 text-white px-2 py-[1px] text-sm  rounded-md" id = "btn-delete" data-id = "<?= $product['id']?>"><i class="fas fa-trash"></i></button>
+                            <td class = "border border-gray-200 text-[11px] md:text-[13px] py-1">
+                                <button class = "bg-blue-500 text-white px-2 py-[1px] text-[11px] md:text-[13px]  rounded-md" id = "productEdit" data-id = "<?= $product['id']?>"><i class="fas fa-edit"></i></button>
+                                <button class = "bg-red-500 text-white px-2 py-[1px] text-[11px] md:text-[13px]  rounded-md" id = "btn-delete" data-id = "<?= $product['id']?>"><i class="fas fa-trash"></i></button>
                             </td>
                             </tr>
                             <?php
