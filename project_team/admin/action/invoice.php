3c705b3c453abf693c64439d2a887d0cba5b6491
morn sovathana <mornsovathana@gmail.com>
2025-05-13 15:01:22 +0700

    update code js in userprofile

--- project_team/admin/action/invoice.php
+++ project_team/admin/action/invoice.php
@@ -6,12 +6,131 @@
 
     $pdf->AddPage();
 
-    $pdf->SetFont("Arial","B",20);
+    $pdf->SetFont("Arial","B",16);
 
-    $pdf->Image('../assets/img/default.jpg',10,10,20,20);
-    $pdf->cell(65,10,"",0,0);
-    $pdf->cell(59,5,"En Lin Smart Phone",0,0);
-    $pdf->cell(59,10,"",0,1);
+    $pdf->Image('../assets/img/default.jpg',15,10,15,15);
+    $pdf->Cell(65,8,"",0,0);
+    $pdf->Cell(59,5,"En Lin Smart Phone",0,0);
+    $pdf->Cell(59,8,"",0,1);
+
+    $pdf->SetFont("Arial","B",10);
+    $pdf->Cell(46,7,"",0,0);
+    $pdf->Cell(59,5,"Prasaut village,Kandengray Commune ,Svay teab district",0,0);
+    $pdf->Cell(59,7,"",0,1);
+
+    $pdf->SetFont("Arial","B",10);
+    $pdf->Cell(75,6,"",0,0);
+    $pdf->Cell(59,4,"Svay rieng province ",0,0);
+    $pdf->Cell(59,6,"",0,1);
+
+    $pdf->SetFillColor(50,55,57);
+    $pdf->Rect(15,32,90,1,'F');
+
+    $pdf->SetFont("Arial","B",11);  
+    $pdf->SetFillColor(255,255,255);
+    $pdf->SetDrawColor(50,55,57);
+    $pdf->SetLineWidth(0.5);
+    $pdf->Rect(15,38,90,15,'DF');
+
+    $pdf->SetFont("Arial","B",10);
+    $pdf->Cell(96,2,"",0,0);
+    $pdf->Cell(59,2,"Form information",0,0);
+    $pdf->Cell(59,2,"",0,1);
+
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(4,5,"",0,0);
+    $pdf->Cell(59,5,"Saleman",0,0);    
+    $pdf->Cell(59,5,"",0,1);
+
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(5,0,"",0,0);
+    $pdf->Cell(59,8,"Name",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    $pdf->SetFont("Arial","",9);
+    $pdf->Cell(40,8,"",0,0);
+    $pdf->Cell(59,8,": En Rathana",0,0);    
+    $pdf->Cell(59,8,"",0,1);
+
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(5,0,"",0,0);
+    $pdf->Cell(59,3,"Phone number",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    $pdf->SetFont("Arial","",9);
+    $pdf->Cell(40,0,"",0,0);
+    $pdf->Cell(59,3,": 088 97 97 022",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    // 
+
+    $pdf->SetFont("Arial","B",11);
+    $pdf->SetFillColor(255,255,255);
+    $pdf->SetDrawColor(50,55,57);
+    $pdf->SetLineWidth(0.5);
+    $pdf->Rect(106,38,90,15,'DF');
+
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(96,5,"",0,0);
+    $pdf->Cell(59,-21,"Customer",0,0);    
+    $pdf->Cell(59,5,"",0,1);
+
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(96,0,"",0,0);
+    $pdf->Cell(59,-20,"Name",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    $pdf->SetFont("Arial","",9);
+    $pdf->Cell(130,8,"",0,0);
+    $pdf->Cell(59,-20,": Morn sovathana",0,0);    
+    $pdf->Cell(59,8,"",0,1);
+
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(96,0,"",0,0);
+    $pdf->Cell(59,-27,"Date",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    $pdf->SetFont("Arial","",9);
+    $pdf->Cell(130,0,"",0,0);
+    $pdf->Cell(59,-27,": 23/02/2025 10:00",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(96,0,"",0,0);
+    $pdf->Cell(59,-18,"Status",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    $pdf->SetFont("Arial","",9);
+    $pdf->Cell(130,0,"",0,0);
+    $pdf->Cell(59,-18,": Finished",0,0);    
+    $pdf->Cell(59,0,"",0,1);
+
+    // table 
+    $pdf->SetFont("Arial","B",9);
+    $pdf->SetDrawColor(50,55,57);
+    $pdf->Cell(5);
+    $pdf->Cell(30,6,'ID',1,0,'C');
+    $pdf->Cell(30,6,'Description',1,0,'C');
+    $pdf->Cell(30,6,'Qty',1,0,'C');
+    $pdf->Cell(30,6,'Unit Price',1,0,'C');
+    $pdf->Cell(30,6,'Payment Method',1,0,'C');
+    $pdf->Cell(30,6,'Total',1,1,'C');
+
+    $pdf->SetFont("Arial","",9);
+    $pdf->Cell(5);
+    $pdf->Cell(30,6,'ID',1,0,'L');
+    $pdf->Cell(30,6,'Description',1,0,'L');
+    $pdf->Cell(30,6,'Qty',1,0,'R');
+    $pdf->Cell(30,6,'Unit Price',1,0,'R');
+    $pdf->Cell(30,6,'Sales Tax',1,0,'R');
+    $pdf->Cell(30,6,'Total',1,1,'R');
+
+    // 
+    $pdf->SetFont("Arial","B",9);
+    $pdf->Cell(125,6,'',0,0);
+    $pdf->Cell(30,6,"Subtotal",0,0,'C');
+    $pdf->Cell(30,6,"100,000",1,0,'R');
+    
 
     $pdf->Output();
 
---
+++ project_team/admin/assets/js/userreport.js
@@ -0,0 +1,73 @@
+ $(document).ready(function(){
+        let currentPage = 1;
+        let limit = 10;
+        let startPage = $("#startPage");
+        let totalPage = $("#totalPage");
+
+        $("#page_num").change(function(){
+            limit = parseInt($(this).val());
+            currentPage = 1;
+            startPage.text(currentPage);
+            load(currentPage);
+        });
+
+
+        $("#next_btn").click(function(){
+            const totalPageValue = parseInt(totalPage.text());
+            if(currentPage < totalPageValue){
+                currentPage += 1;
+                load(currentPage);
+                startPage.text(currentPage);
+            }
+            
+        });
+
+        $("#back_btn").click(function(){
+            if(currentPage > 1){
+                currentPage -= 1;
+                load(currentPage);
+                startPage.text(currentPage);
+            }
+            
+        });
+
+        // function 
+
+        function load(page){
+            const display = $("#displayData");
+            const id = $("#userId").val();
+            $.ajax({
+                method: "POST",
+                url: "action/userReport.php",
+                data: {
+                    "page": page,
+                    "limit": limit,
+                    "userId" : id
+                },
+                dataType: "json",
+                beforeSend: function(){
+                    display.html(`<span class="loader absolute left-[50%]"></span>`);
+                },
+                success: function (data) {
+                    if(data.length > 0){
+                        let txt = "";
+                        for(i in data){
+                            let item = data[i];
+                            txt += `<tr id="brand-${item.id}">
+                            <td class="text-[11px] shadow-style bg-[#ffffff] md:text-[13px] py-1">${item.id}</td>
+                            <td class="text-[11px] shadow-style bg-[#ffffff] md:text-[13px] py-1">${item.name}</td>
+                             <td class="text-[11px] shadow-style bg-[#ffffff] md:text-[13px] py-1">${item.product}</td>
+                            <td class="text-[11px] shadow-style bg-[#ffffff] md:text-[13px] py-1">${item.price}</td>
+                            <td class="text-[11px] shadow-style bg-[#ffffff] md:text-[13px] py-1">${item.created}</td>
+                            </tr>`;
+                        }
+                        totalPage.text(Math.ceil( data[0]['total'] / limit));
+                        display.html(txt);
+                    }else{
+                        display.html(`<tr><td colspan = "6">No Data</td></tr>`);
+                    }
+                }
+            });
+        }
+        load(currentPage);
+    });
