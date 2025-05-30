6e7f9dc15f1e3129cc453d00dfc4672a410662e2
morn sovathana <mornsovathana@gmail.com>
2025-05-30 16:44:42 +0700

    fix search value

--- project_team/action/searchProduct.php
+++ project_team/action/searchProduct.php
@@ -7,7 +7,7 @@
         $name = $_POST['name'];
         $search = "%{$name}%";
 
-        $stmt = $conn->prepare("SELECT d.id,d.brand_id,d.promotion,d.barcode,d.name,d.original_price,d.sell_price 
+        $stmt = $conn->prepare("SELECT d.id,d.brand_id,d.slug,d.barcode,d.name,d.original_price,d.sell_price 
                     , i.specification,i.description,i.image,i.demo_image
                      FROM product_detail d JOIN product_image i on d.id = i.product_id
                       where d.name LIKE ?");
