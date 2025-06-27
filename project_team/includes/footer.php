3ab692a61a03f0a8324eda2753a5f38a7cce1936
mroun123 <mrounit83@gmail.com>
2025-06-27 21:02:46 +0700

    new

--- project_team/includes/footer.php
+++ project_team/includes/footer.php
@@ -116,32 +116,44 @@
       <!-- Call an agent -->
       <div class="flex items-center space-x-2">
         <span class="bg-gray-700 text-white text-sm px-2 py-1 rounded">Call an agent</span>
-        <button class="bg-blue-600 hover:bg-green-700 p-3 rounded-full shadow-lg">
-          📞
+        <button class="bg-blue-600 hover:bg-green-700 p-3 rounded-full shadow-lg text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+        </svg>
+
         </button>
       </div>
 
       <!-- Live chat -->
       <div class="flex items-center space-x-2">
-        <span class="bg-gray-700 text-white text-sm px-2 py-1 rounded">Live chat</span>
-        <button class="bg-blue-600 hover:bg-green-700 p-3 rounded-full shadow-lg">
-          💬
+        <span class="bg-gray-700 text-white text-sm px-2 py-1 rounded">Telegram</span>
+        <button class="bg-blue-600 hover:bg-green-700 p-3 rounded-full shadow-lg text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+        </svg>
+
         </button>
       </div>
 
       <!-- Send email -->
       <div class="flex items-center space-x-2">
         <span class="bg-gray-700 text-white text-sm px-2 py-1 rounded">Send email</span>
-        <button class="bg-blue-600 hover:bg-green-700 p-3 rounded-full shadow-lg">
-          ✉️
+        <button class="bg-blue-600 hover:bg-green-700 p-3 rounded-full shadow-lg text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+        </svg>
+
         </button>
       </div>
     </div>
 
     <!-- Toggle Button -->
-    <button id="toggleMenuBtn" class="bg-gray-700 text-white p-4 rounded-full shadow-lg hover:bg-gray-800 transition w-12 h-12 flex items-center justify-center">
+    <button id="toggleMenuBtn" class="bg-blue-500 text-white p-4 rounded-full shadow-lg hover:bg-gray-800 transition w-12 h-12 flex items-center justify-center">
       <!-- Icon changes here -->
-      <span id="menuIcon">＋</span>
+      <span id="menuIcon" class="text-2xl"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+      </span>
     </button>
   </div>
 
