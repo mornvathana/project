6129daf1f20acd800e95b01dcffd425a52b63f43
morn sovathana <mornsovathana@gmail.com>
2025-07-03 11:45:26 +0700

    fix brands report

--- project_team/admin/action/getProducts.php
+++ project_team/admin/action/getProducts.php
@@ -12,24 +12,19 @@
     $rTotal = $sTotal->fetch_array();
     $total = $rTotal['0'];
 
-    $order1 = "SELECT d.id,d.brand_id,d.promotion,d.barcode,d.name,d.original_price,d.sell_price 
+    $order1 = "SELECT d.id,d.brand_id,d.slug,d.barcode,d.name,d.original_price,d.sell_price 
               , i.specification,i.description,i.image,i.demo_image,i.created_at 
               FROM product_detail d JOIN product_image i on d.id = i.product_id LIMIT $totalPage,$limit";
     $order = $conn->query($order1);
     $Data = array();
     if ($order->num_rows > 0) {
-        while($row = $order->fetch_array()){
-            $data = $conn->query("SELECT * FROM users WHERE id = 5");
-            foreach( $data as $item){
-                $userName = $item['name'];
-            }
+        while($row = $order->fetch_assoc()){
             $Data[] = array(
-                "id" => $row[1],
-                "barcode" => $row[3],
-                "name" => $row[4],
+                "id" => $row['id'],
+                "barcode" => $row['barcode'],
+                "name" => $row['name'],
                 "total" => $total,
-                "date" => $row[11],
-                "user_id" => $userName
+                "date" => $row['created_at'],
             );
         }
         echo json_encode($Data);
